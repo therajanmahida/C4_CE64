0bca4053c024d583d6f516ce9e442f8bc2494dee
Rajan Mahida <rajanmahida103@gmail.com>
2016-11-21 03:01:46 +0530

    Add files via upload

---
+++ LMS2/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Linq;
+using System.Xml.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+// Review the values of the assembly attributes
+
+[assembly: AssemblyTitle("Library Management System")]
+[assembly: AssemblyDescription("DDU College Prjoect")]
+[assembly: AssemblyCompany("Rajan Mahida")]
+[assembly: AssemblyProduct("CE 64")]
+[assembly: AssemblyCopyright("Mark 1")]
+
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(false)]
+//The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("e638c0e9-44b6-498e-87b4-9713f5c15e50")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// <Assembly: AssemblyVersion("1.0.*")> 
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
